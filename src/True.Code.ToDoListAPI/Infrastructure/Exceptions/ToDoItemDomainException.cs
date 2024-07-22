488ed1f05bb07bbfa56d268ef4473ec72709246d
Dmitry Kosarev <kosarevdmlive@outlook.com>
2024-07-22 08:55:28 +0200

    #: exception

---
+++ src/True.Code.ToDoListAPI/Infrastructure/Exceptions/ToDoItemDomainException.cs
@@ -0,0 +1,23 @@
+﻿namespace True.Code.ToDoListAPI.Infrastructure.Exceptions;
+
+public class ToDoItemDomainException : Exception
+{
+    public ToDoItemDomainException()
+    {
+    }
+
+    public ToDoItemDomainException(Exception ex)
+        : base(ex.Message + " " + ex.InnerException?.Message)
+    {
+    }
+
+    public ToDoItemDomainException(string message)
+        : base(message)
+    {
+    }
+
+    public ToDoItemDomainException(string message, Exception innerException)
+        : base(message, innerException)
+    {
+    }
+}
