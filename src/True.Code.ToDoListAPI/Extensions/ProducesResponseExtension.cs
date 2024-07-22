48bd181e7be07453bba0f721f2695a5084b97cf7
Dmitry Kosarev <kosarevdmlive@outlook.com>
2024-07-22 08:55:04 +0200

    #: extensions

---
+++ src/True.Code.ToDoListAPI/Extensions/ProducesResponseExtension.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http.Features;
+using Swashbuckle.AspNetCore.Annotations; //additional nuget package
+using True.Code.ToDoListAPI.Helper;
+
+namespace True.Code.ToDoListAPI.Extensions;
+
+public static class ProducesResponseExtension
+{
+    public static IApplicationBuilder UseProducesOrSwaggerResponseCheck(this IApplicationBuilder app)
+    {
+        return app.UseMiddleware<SwaggerResponseCheck>();
+    }
+}
+
+public class SwaggerResponseCheck
+{
+    private readonly RequestDelegate _next;
+
+    public SwaggerResponseCheck(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        using (var buffer = new MemoryStream())
+        {
+            var stream = context.Response.Body;
+            context.Response.Body = buffer;
+            await _next(context);
+
+            var metadata = context.Features.Get<IEndpointFeature>()?.Endpoint?.Metadata;
+            IEnumerable<ProducesResponseTypeAttribute> attributes = null;
+            if (metadata != null)
+            {
+                attributes = metadata.OfType<SwaggerResponseAttribute>()
+                    .Union(metadata.OfType<ProducesResponseTypeAttribute>());
+            }
+
+            //  affect only for Action with relevant attributes
+            if (attributes.Count() != 0 && !attributes.Any(a => a.StatusCode == context.Response.StatusCode))
+            {
+                context.Response.StatusCode = 500;
+                buffer.Seek(0, SeekOrigin.Begin);
+                await context.Response.WriteAsync(
+                    $"OpenAPI specification exception, unsupported status code: {context.Response.StatusCode}\npath: {context.Request.Path}");
+            }
+
+            buffer.Seek(0, SeekOrigin.Begin);
+            await context.Response.Body.CopyToAsync(stream);
+            context.Response.Body = stream;
+        }
+    }
+}
---
+++ src/True.Code.ToDoListAPI/Extensions/ProgramExtension.cs
@@ -0,0 +1,22 @@
+﻿using Serilog;
+
+namespace True.Code.ToDoListAPI.Extensions
+{
+    public static class ProgramExtension
+    {
+        public static void CreateSerilogLogger(this WebApplicationBuilder builder)
+        {
+            const string appName = "ToDoListAPI";
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Verbose()
+                .Enrich.WithProperty("ApplicationContext", appName)
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .WriteTo.File("log.txt", rollingInterval: RollingInterval.Day)
+                .ReadFrom.Configuration(builder.Configuration)
+                .CreateLogger();
+
+            builder.Host.UseSerilog();
+        }
+    }
+}
