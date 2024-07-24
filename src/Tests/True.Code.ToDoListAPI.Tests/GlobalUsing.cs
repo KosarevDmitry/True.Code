848c07c6981fab47b3d74473637787b2df7c5b55
Dmitry Kosarev <kosarevdmlive@outlook.com>
2024-07-24 07:09:40 +0200

    #: globalusing

---
+++ src/Tests/True.Code.ToDoListAPI.Tests/GlobalUsing.cs
@@ -0,0 +1,10 @@
+﻿global  using System.Text;
+global  using Microsoft.AspNetCore.Http;
+global  using Microsoft.AspNetCore.Mvc.Testing;
+global  using Microsoft.EntityFrameworkCore;
+global  using Microsoft.Extensions.DependencyInjection;
+global  using Microsoft.Extensions.DependencyInjection.Extensions;
+global  using Newtonsoft.Json;
+global  using True.Code.ToDoListAPI.Models;
+global  using True.Code.ToDoListAPI.Models.CTOs;
+global  using True.Code.ToDoListAPI.Data;
