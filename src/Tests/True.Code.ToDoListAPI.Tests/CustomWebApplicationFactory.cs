c7f48c52d379566b0665c3258719e9b4355011e3
Dmitry Kosarev <kosarevdmlive@outlook.com>
2024-07-24 07:17:41 +0200

    #: кастомизацияи webappfactory для тестирования не подключенная

---
+++ src/Tests/True.Code.ToDoListAPI.Tests/CustomWebApplicationFactory.cs
@@ -0,0 +1,51 @@
+using System.Data.Common;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Data.SqlClient;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using True.Code.ToDoListAPI.Data;
+
+namespace True.Code.ToDoListAPI.Tests;
+
+
+public class CustomWebApplicationFactory<TProgram>
+    : WebApplicationFactory<TProgram> where TProgram : class
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            var dbContextDescriptor = services.SingleOrDefault(
+                d => d.ServiceType ==
+                    typeof(DbContextOptions<ToDoItemDbContext>));
+
+            if (dbContextDescriptor != null) services.Remove(dbContextDescriptor);
+
+            var dbConnectionDescriptor = services.SingleOrDefault(
+                d => d.ServiceType ==
+                    typeof(DbConnection));
+
+            if (dbConnectionDescriptor != null) services.Remove(dbConnectionDescriptor);
+
+            // Create open Connection so EF won't automatically close it.
+            services.AddSingleton<DbConnection>(container =>
+            {
+                var connection = new SqlConnection(@"Server=(localdb)\mssqllocaldb;Database=ToDoItemDb;Trusted_Connection=True");
+                connection.Open();
+
+                return connection;
+            });
+
+            services.AddDbContext<ToDoItemDbContext>((container, options) =>
+            {
+                var connection = container.GetRequiredService<DbConnection>();
+                options.UseSqlServer(connection);
+            });
+        });
+
+        builder.UseEnvironment("Development");
+    }
+}
+
