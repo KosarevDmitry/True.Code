6534007ea22ac771fa55b6f48c1fba87655ca2f9
Dmitry Kosarev <kosarevdmlive@outlook.com>
2024-07-24 07:12:14 +0200

    #: refactoring

--- src/True.Code.ToDoListAPI/Extensions/ProducesResponseExtension.cs
+++ src/True.Code.ToDoListAPI/Extensions/ProducesResponseExtension.cs
@@ -30,7 +30,7 @@ public class SwaggerResponseCheck
             await _next(context);
 
             var metadata = context.Features.Get<IEndpointFeature>()?.Endpoint?.Metadata;
-            IEnumerable<ProducesResponseTypeAttribute> attributes = null;
+            IEnumerable<ProducesResponseTypeAttribute>? attributes = null;
             if (metadata != null)
             {
                 attributes = metadata.OfType<SwaggerResponseAttribute>()
@@ -38,14 +38,18 @@ public class SwaggerResponseCheck
             }
 
             //  affect only for Action with relevant attributes
-            if (attributes.Count() != 0 && !attributes.Any(a => a.StatusCode == context.Response.StatusCode))
+            if (attributes != null)
             {
-                context.Response.StatusCode = 500;
-                buffer.Seek(0, SeekOrigin.Begin);
-                await context.Response.WriteAsync(
-                    $"OpenAPI specification exception, unsupported status code: {context.Response.StatusCode}\npath: {context.Request.Path}");
+                var list = attributes.ToList();
+                if (list.Count() != 0 && list.Any(a => a.StatusCode == context.Response.StatusCode))
+                {
+                    context.Response.StatusCode = 500;
+                    buffer.Seek(0, SeekOrigin.Begin);
+                    await context.Response.WriteAsync(
+                        $"OpenAPI specification exception, unsupported status code: {context.Response.StatusCode}\npath: {context.Request.Path}");
+                }
             }
-
+            
             buffer.Seek(0, SeekOrigin.Begin);
             await context.Response.Body.CopyToAsync(stream);
             context.Response.Body = stream;
